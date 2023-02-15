c2be09ec702c098ba1229b8edece266cbb9bb7df
EstherABA <120097060+EstherABA@users.noreply.github.com>
2023-02-15 13:07:37 +0200

    adding meaning icons and their css

--- src/components/Home Page/HomePage.tsx
+++ src/components/Home Page/HomePage.tsx
@@ -4,15 +4,16 @@ import './HomePage.css'
 import Hero from '../Hero/Hero';
 import PopularRestaurant from '../Popular Restaurant/PopularRestaurant'
 import PopularDish from '../PopularDish/PopularDish';
+import MeaningOfIcon from '../Meaning of icon/MeaningOfIcon';
 
 const HomePage: React.FC= () => {
     return (
         <div>
             <NavBar />
             <Hero />
-            <PopularRestaurant/>
+            <PopularRestaurant />
             <PopularDish />
-            <div className='popular dish'></div>
+            < MeaningOfIcon />
         </div>
 
     )
