80aee91371b19cd1be7c4882628ae77555d1e8ee
EstherABA <120097060+EstherABA@users.noreply.github.com>
2023-02-16 15:57:35 +0200

    bar css

--- src/components/RestaurantsPage/RestaurantsPage.tsx
+++ src/components/RestaurantsPage/RestaurantsPage.tsx
@@ -5,28 +5,15 @@ import HomeCard from '../General/HomeCard/HomeCard';
 import NavBar from '../NavBar/NavBar';
 import FilterBarByNew from '../General/FilterBarByNew/FIlterBarByNew';
 import FilterBarByPrice from '../General/FilterBarByPrice/FilterBarByPrice'
-// import onza from '../../assets/images/onza.png'
 
 const RestaurantsPage: React.FC= () => {
-//     const allRestaurant = data.restaurants.map((restaurant)=>{
-        
-//         return <HomeCard 
-//         // class='restaurantP-card'
-//         ImgSrc={require(`../../assets/images/${restaurant.image}`).default} 
-//         ImgAlt={restaurant.name}  
-//         name={restaurant.name} 
-//         chefName={restaurant.chefName} 
-//         moreInfoSrc={require(`../../assets/icon/star${restaurant.rating}.svg`)}
-//       />}
-//   );
-
 
     return (
         <div>
             <NavBar />
             <FilterBarByNew />
             <FilterBarByPrice />
-         </div>
+        </div>
 
     )
 }
