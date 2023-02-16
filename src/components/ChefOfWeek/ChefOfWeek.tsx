aa6f2f7c145e26e5d29e8051e0f30deb34ad9624
EstherABA <120097060+EstherABA@users.noreply.github.com>
2023-02-16 12:10:58 +0200

    adding store and slice - not complete!

--- src/components/ChefOfWeek/ChefOfWeek.tsx
+++ src/components/ChefOfWeek/ChefOfWeek.tsx
@@ -10,19 +10,23 @@ const ChefOfWeek: React.FC = () => {
     const chefs = data.chefs.map((chef) => { 
         return ( 
            chef.isChefOfTheWeek ? (
-          <div className='chefs-data-container'>
-            <ChefPortraitCard 
-            src={require(`../../${chef.portrait}`)} alt={`${chef.name}`} name={`${chef.name}`} description={`${chef.description}`} />
-            <p className=' title-of-the-week'>{`${chef.name} Restaurants:` } </p>
+            <div className='chefs-data-container'>
+                <ChefPortraitCard 
+                    src={require(`../../${chef.portrait}`)} 
+                    alt={`${chef.name}`} 
+                    name={`${chef.name}`} 
+                    description={`${chef.description}`} 
+                    />
+
+                <p className=' title-of-the-week'>{`${chef.name} Restaurants:` } </p>
                 <div className='chef-restaurants'>
                     { 
-                    chef.restaurants.map((res)=> <HomeCard ImgSrc={require(`../../${data.restaurants[3].image}`)} ImgAlt={res} name={res} />)
+                        chef.restaurants.map((restaurant)=> <HomeCard ImgSrc={require(`../../${data.restaurants[3].image}`)} ImgAlt={restaurant} name={restaurant} />)
                     }
                 </div>
-            </div>) :
-            (<></>)
+            </div>) : (<></>)
         )
-                });
+    });
 
     return (  
         <div>
